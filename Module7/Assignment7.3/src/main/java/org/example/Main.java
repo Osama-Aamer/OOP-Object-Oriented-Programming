b7f5dae27f5e1ce5901ad2d0ac5e47ea47c29e95
Osama-Aamer <osamaaamer2@gmail.com>
2025-12-14 23:52:33 +0200

    All Tasks/items for OOP Assignment 7.3.. done

---
+++ Module7/Assignment7.3/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javafx.application.Application;
+import org.example.view.CurrencyView;
+//
+public class Main {
+
+    public static void main(String[] args){
+        Application.launch(CurrencyView.class,args);
+
+    }
+}
+
